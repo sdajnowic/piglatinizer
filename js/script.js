418d193c9146f95b7d2400ca832c6816b8c95617
sdajnowic <steven.dajnowicz@schrodinger.com>
2019-10-28 17:33:02 +0000

    added compute sum function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("button").click(function(){ // click handler
+    $(".output").text("");
+    var nums =$("input").val(); // store the input into var num
+    var nums_array = nums.split('+'); // split number by +
 
+    var sum = 0; //
+    nums_array.forEach(function(element){
+        sum += element;
+    });
 
+    $('.output').append(nums+" = "+sum);
 
-  
-  
-  
-  
+});
 });
